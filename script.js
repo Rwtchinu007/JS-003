89a1864b9d8817ed7482d5c4ffb41e03dc66785c
Kshitiz Rawat <rwtchinu007@gmail.com>
2025-10-27 22:07:55 +0530

    operators in js

---
+++ script.js
@@ -0,0 +1,58 @@
+// we will learn about operators in js
+// operators are used to perform operations on variables and values
+
+// arithmetic operators
+let a = 10;
+let b = 5;
+// let sum = a + b; // addition
+// let diff = a - b; // subtraction
+// let prod = a * b; // multiplication
+// let quot = a / b; // division
+// let mod = a % b;  // modulus
+// let exp = a ** b; // exponentiation
+
+// comparison operators
+//  a == b; // equal to
+//  a != b; // not equal to
+//  a === b; // strict equal to
+//  a !== b; // strict not equal to
+//  a > b; // greater than
+//  a < b; // less than
+//  a >= b; // greater than or equal to
+//  a <= b; // less than or equal to  
+
+// assignment operator
+//  a = 15; // assignment
+//  a += 5; // a = a + 5
+//  a -= 5; // a = a - 5
+//  a *= 2; // a = a * 2
+//  a /= 2; // a = a / 2
+//  a %= 3; // a = a % 3
+
+// logical operators
+let x = true;
+let y = false;
+// let andOp = x && y; // logical AND
+// let orOp = x || y; // logical OR
+// let notOp = !x; // logical NOT
+// to find the real nature of variables we use !! to find its truthy or falsy value
+
+// unary operators
+let c = +"39"; // unary plus
+name2 = +"John"; // NaN because "John" is not a number
+// (!) is also a unary operator it is used to negate a boolean value
+// ++ and -- are also unary operators used to increment or decrement a value
+// pre increment is when the operator is before the variable ++a
+// post increment is when the operator is after the variable a++ 
+
+// ternary operator
+let age = 18;
+let canVote = (age >= 18) ? "Yes" : "No"; // if age is greater than or equal to 18 then "Yes" else "No"
+
+// typeof operator
+let typeOfA = typeof a; // returns the type of variable a
+
+// instanceof operator
+arr1 = [1, 2, 3];
+// let isArray = arr1 instanceof Array; // returns true if arr1 is an instance of Array
+
